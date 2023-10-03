e946e4ca7aee50eee334e7828a480dcc93aff469
mararo-ops <danielmararo1@gmail.com>
2023-10-04 01:39:58 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({ name:name, email: email})
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      document.body.innerHTML += `<p>${error.message}</p>`;
+    });
+  }
+ 
+ 
